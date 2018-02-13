65a630d8ea3e6f4b5cbd82b1ca1eb39f8c3691fb
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-13 14:35:27 -0700

    update clientId on reviews.sql table

--- db/reviews.sql
+++ db/reviews.sql
@@ -6,8 +6,14 @@ CREATE TABLE public.reviews
 (
     review_id bigint NOT NULL DEFAULT nextval('reviews_review_id_seq'::regclass),
     "clientId" bigint NOT NULL,
+    "caregiverId" bigint NOT NULL,
     message character varying(1000) COLLATE pg_catalog."default",
     rating character varying(100) COLLATE pg_catalog."default",
+    CONSTRAINT reviews_pkey PRIMARY KEY (review_id),
+    CONSTRAINT "caregiverId" FOREIGN KEY (review_id)
+        REFERENCES public.caregivers ("caregiverId") MATCH SIMPLE
+        ON UPDATE CASCADE
+        ON DELETE CASCADE,
     CONSTRAINT "clientId" FOREIGN KEY (review_id)
         REFERENCES public.clients ("clientId") MATCH SIMPLE
         ON UPDATE CASCADE
