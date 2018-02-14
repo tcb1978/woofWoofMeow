c4c87999ee318c78386a549712e1b00c8f294c19
Sergey Sherstobitov <in43sh@gmail.com>
2018-02-14 14:14:57 -0700

    Caregivers controller done

--- server/controllers/caregivers_controller.js
+++ server/controllers/caregivers_controller.js
@@ -1,13 +1,29 @@
 module.exports = {
   getAll: (req, res, next) => {
-    
+    const db = req.app.get('db');
+
+    db.get_all_caregivers([])
+      .then( (caregivers) => res.status(200).json(caregivers) )
+      .catch( (error) => res.status(500).send(error) )
   },
 
   getOne: (req, res, next) => {
-    
+    const db = req.app.get('db');
+    const { id } = req.params;
+
+    db.get_caregiver([ id ])
+      .then( (caregiver) => res.status(200).json(caregiver) )
+      .catch( (error) => res.status(500).send(error) );
   },
 
   destroy: (req, res, next) => {
-    
+    const db = req.app.get('db');
+    console.log(req.params);
+    const { id } = req.params;
+    console.log(id);
+
+    db.delete_caregiver([ id ])
+      .then( () => res.status(200).json('deleted') )
+      .catch( (error) => res.status(500).send(error) );
   }
 }
