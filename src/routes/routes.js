cf733d22903d551ad6136c7f2ba131e5781d559c
Sergey Sherstobitov <in43sh@gmail.com>
2018-02-16 15:01:08 -0700

    Fixing some small bugs

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Layout from '../containers/Layout/Layout';
 import Signin from '../components/Signin/Signin';
-import Petowner from '../containers/Profile/Petowner/Petowner';
-import Caregiver from '../containers/Profile/Caregiver/Caregiver';
 import Profile from '../containers/Profile/Profile';
 
 export default (
