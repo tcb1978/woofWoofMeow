90f01351ebb92dcf70df76e3595e23502cf71204
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-17 16:29:23 -0700

    update reviews table and add design png to assets folder

--- db/reviews.sql
+++ db/reviews.sql
@@ -4,15 +4,15 @@
 
 CREATE TABLE reviews
 (
-    review_id bigint NOT NULL DEFAULT nextval('reviews_review_id_seq'::regclass),
-    user_id bigint NOT NULL,
-    message character varying(1000) COLLATE pg_catalog."default",
-    rating character varying(10) COLLATE pg_catalog."default",
-    job_id bigint NOT NULL,
-    CONSTRAINT user_id FOREIGN KEY (review_id)
-        REFERENCES users (user_id) MATCH SIMPLE
-        ON UPDATE CASCADE
-        ON DELETE CASCADE
+    review_id integer NOT NULL DEFAULT nextval('reviews_review_id_seq'::regclass),
+    user_id integer NOT NULL,
+    message character varying(1000) COLLATE pg_catalog."default" NOT NULL,
+    rating character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    job_id integer NOT NULL,
+    day character varying(10) COLLATE pg_catalog."default" NOT NULL DEFAULT 1,
+    month character varying(20) COLLATE pg_catalog."default" NOT NULL DEFAULT 1,
+    year character varying(4) COLLATE pg_catalog."default" NOT NULL DEFAULT 2018,
+    CONSTRAINT reviews_pkey PRIMARY KEY (review_id)
 )
 WITH (
     OIDS = FALSE
