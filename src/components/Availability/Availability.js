4746aefcdbbea2e7f2e5e767fd68aee6bb1e65a4
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-18 16:04:09 -0700

    jobs compnent production

--- src/components/Availability/Availability.js
+++ src/components/Availability/Availability.js
@@ -7,8 +7,59 @@ class Availability extends Component {
         return (
             <Aux>                
                 <div className="Availablity">
-                    <div className="container">
-                        <h1>Availability</h1>
+                    <h1>Availability</h1>
+                    <div className="WeekLongCalendar">
+                        <div className="WeekdayHeader">
+                            <div className="Times">
+                                <label className="Day left-radius tab-radius"><span>Monday</span></label>
+                                <div className="border-left border-bottom border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                            <div className="Times">
+                                <label className="Day tab-radius"><span>Tuesday</span></label>
+                                <div className="border-left border-bottom border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                            <div className="Times">
+                                <label className="Day tab-radius"><span>Wednesday</span></label>
+                                <div className="border-left border-bottom border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                            <div className="Times">
+                                <label className="Day tab-radius"><span>Thursday</span></label>
+                                <div className="border-left border-bottom border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                            <div className="Times">
+                                <label className="Day tab-radius"><span>Friday</span></label>
+                                <div className="border-left border-bottom border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                            <div className="Times">
+                                <label className="Day tab-radius"><span>Saturday</span></label>
+                                <div className="border-left border-bottom border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                            <div className="Times">
+                                <label className="Day right-radius tab-radius"><span>Sunday</span></label>
+                                <div className="border-left border-bottom border-right border mobile-radius">
+                                    <span classNames="hours">6:00-2:00</span>
+                                    <span classNames="am-pm">AM</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Aux>
