4006643ebda99620f9a2c6cd767c8a1e37b88edf
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-19 15:39:29 -0700

    pet bio production

--- src/containers/Profile/Petowner/Petowner.js
+++ src/containers/Profile/Petowner/Petowner.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
-import Aux from '../../../hoc/Aux'
+import Aux from '../../../hoc/Aux';
+import PetBio from '../../../components/PetBio/PetBio'
 
 export default class Petowner extends Component {
   render() {
     return (
-      <Aux>haha petowner!</Aux>
+      <Aux>
+        <PetBio />
+
+      </Aux>
     );
   }
 }
