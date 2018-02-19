dda32b65231fcac35aeef7f4ef86301239a1fa92
Matthew Eldredge <mattheweldredge@gmail.com>
2018-02-19 15:41:55 -0700

    add bootstraping to petBio.js

--- src/components/PetBio/PetBio.js
+++ src/components/PetBio/PetBio.js
@@ -21,23 +21,27 @@ class PetBio extends Component {
                         </div>
                     </div>
                     <div className="PetBioDropDown">
-                        <div className="column-style">
-                            <div className="col-xs-12 col-sm-4">
-                                <div className="PetBioDropDownHeader">
-                                    <div className="Avatar"></div>
-                                    <div className="Name"><h1>Berry</h1></div>
+                        <div className="container">
+                            <div className="row">
+                                <div className="column-style">
+                                    <div className="col-xs-12 col-sm-4">
+                                        <div className="PetBioDropDownHeader">
+                                            <div className="Avatar"></div>
+                                            <div className="Name"><h1>Berry</h1></div>
+                                        </div>
+                                    </div>
+                                    <div className="col-xs-12 col-sm-4">
+                                        <ul className="PetDetailsList">
+                                            <li>Breed <span>    Hound</span></li>
+                                            <li>Age <span>  6 years</span></li>
+                                            <li>Weight <span>   60 lbs.</span></li>
+                                            <li>Sex <span>  Female</span></li>
+                                        </ul>
+                                    </div>
+                                    <div className="col-xs-12 col-sm-4">
+                                        <div>minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="col-xs-12 col-sm-4">
-                            <ul className="PetDetailsList">
-                                <li>Breed <span>    Hound</span></li>
-                                <li>Age <span>  6 years</span></li>
-                                <li>Weight <span>   60 lbs.</span></li>
-                                <li>Sex <span>  Female</span></li>
-                            </ul>
-                            </div>
-                            <div className="col-xs-12 col-sm-4">
-                            <div>minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
                             </div>
                         </div>
                     </div>
