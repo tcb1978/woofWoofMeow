845008f67748a51d406c6db97a9f44995326fc57
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-02-20 02:47:19 -0700

    Started the available times section under the calendar

--- src/components/Calendar/AvailableTimes/AvailableTimes.js
+++ src/components/Calendar/AvailableTimes/AvailableTimes.js
@@ -9,10 +9,26 @@ class AvailableTimes extends Component {
 
     render () {
         return (
-            <div className="availableTimes">
+            <div className="available-times">
                 <div className="container">
 
-                    <h3>Available Times</h3>
+                    {/* <h3>Available Times</h3> */}
+
+                    <div className="month-day">
+                        <div className="day">Wednesday</div>
+                        <div className="date">January 9, 2018</div>
+                    </div>
+
+                    <ul>
+                        <li>
+                            <div className="time">2:30pm - 3:00pm</div>
+                            <button className="">Request</button>
+                        </li>
+                        <li>
+                            <div className="time">3:30pm - 4:00pm</div>
+                            <button className="">Request</button>
+                        </li>
+                    </ul>
 
                 </div>
             </div>
