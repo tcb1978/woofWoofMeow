97081c569e956c5fec0a68fe3434156fd831effd
Sergey Sherstobitov <in43sh@gmail.com>
2018-02-21 11:04:52 -0700

    Availability done

--- src/components/Caregiver/Availability/Availability.js
+++ src/components/Caregiver/Availability/Availability.js
@@ -8,20 +8,155 @@ class Availability extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            user_id: 1,
-            month: '',
+            user_id: 2,
             day: '',
             begin_time: '',
             end_time: '',
-            am_pm: '',
-            monday: ''
+            monday: '',
+            tuesday: '',
+            wednesday: '',
+            thursday: '',
+            friday: '',
+            saturday: '',
+            sunday: ''
         }
     }
 
-    handleChange(property, event) {
+    componentDidMount() {
+        const { user_id } = this.state;
+        axios.get('/available/user', {
+            user_id: user_id
+        })
+        .then(response => {
+            console.log(response);
+            const arr = response.data;
+            let monday = arr.filter(weekday => weekday.day === 0)
+            this.setState({ monday: monday[0].time_range })
+            console.log(this.state.monday);
+
+            let tuesday = arr.filter(weekday => weekday.day === 1)
+            this.setState({ tuesday: tuesday[0].time_range })
+            console.log(this.state.tuesday);
+
+            let wednesday = arr.filter(weekday => weekday.day === 2)
+            this.setState({ wednesday: wednesday[0].time_range })
+            console.log(this.state.wednesday);
+
+            let thursday = arr.filter(weekday => weekday.day === 3)
+            this.setState({ thursday: thursday[0].time_range })
+            console.log(this.state.thursday);
+
+            let friday = arr.filter(weekday => weekday.day === 4)
+            this.setState({ friday: friday[0].time_range })
+            console.log(this.state.friday);
+
+            let saturday = arr.filter(weekday => weekday.day === 5)
+            this.setState({ saturday: saturday[0].time_range })
+            console.log(this.state.saturday);
+
+            let sunday = arr.filter(weekday => weekday.day === 6)
+            this.setState({ sunday: sunday[0].time_range })
+            console.log(this.state.sunday);
+
+        })
+        .catch(error => console.log(error))
+    }
+
+    // handleChange(property, event) {
+    //     event.preventDefault();
+    //     this.setState({ [property]: event.target.value })
+    //     axios.update('/update/available', {
+    //         day: 0,
+    //         begin_time: '',
+    //         end_time,
+    //         am_pm,
+    //         user_id
+    //     })
+    // }
+
+    // think about how we make it one function
+    handleMondayChange(event) {
         event.preventDefault();
-        this.setState({ [property]: event.target.value })
-        
+        this.setState({ monday: event.target.value })
+        axios.put('/update/available', {
+            day: 0,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+
+    handleTuesdayChange(event) {
+        event.preventDefault();
+        this.setState({ tuesday: event.target.value })
+        axios.put('/update/available', {
+            day: 1,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+
+    handleWednesdayChange(event) {
+        event.preventDefault();
+        this.setState({ wednesday: event.target.value })
+        axios.put('/update/available', {
+            day: 2,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+
+    handleThursdayChange(event) {
+        event.preventDefault();
+        this.setState({ thursday: event.target.value })
+        axios.put('/update/available', {
+            day: 3,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+
+    handleFridayChange(event) {
+        event.preventDefault();
+        this.setState({ friday: event.target.value })
+        axios.put('/update/available', {
+            day: 4,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+
+    handleSaturdayChange(event) {
+        event.preventDefault();
+        this.setState({ saturday: event.target.value })
+        axios.put('/update/available', {
+            day: 5,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+    }
+
+    handleSundayChange(event) {
+        event.preventDefault();
+        this.setState({ sunday: event.target.value })
+        axios.put('/update/available', {
+            day: 6,
+            time_range: event.target.value,
+            user_id: 2
+        })
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
     }
 
     render() {
@@ -35,32 +170,36 @@ class Availability extends Component {
                                 <label className="Day left-radius tab-radius"><span>Monday</span></label>
                                 <div className="border-left border-bottom border mobile-radius bottom-left-radius">
                                     <select
-                                        className="hours"
+                                        className="form-control"
                                         type="text"
-                                        onChange={(event) => this.handleChange("monday", event)}>
+                                        value={this.state.monday}
+                                        onChange={(event) => this.handleMondayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
                                     </select>
-                                    <p>{this.state.monday}</p>
                                 </div>
                             </div>
                             <div className="Times">
                                 <label className="Day tab-radius"><span>Tuesday</span></label>
                                 <div className="border-left border-bottom border mobile-radius">
                                     <select
-                                        className="hours"
-                                        type="text">
+                                        className="form-control"
+                                        type="text"
+                                        value={this.state.tuesday}
+                                        onChange={(event) => this.handleTuesdayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
-                                </select>
+                                    </select>
                                 </div>
                             </div>
                             <div className="Times">
                                 <label className="Day tab-radius"><span>Wednesday</span></label>
                                 <div className="border-left border-bottom border mobile-radius">
                                     <select
-                                        className="hours"
-                                        type="text">
+                                        className="form-control"
+                                        type="text"
+                                        value={this.state.wednesday}
+                                        onChange={(event) => this.handleWednesdayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
                                     </select>
@@ -70,8 +209,10 @@ class Availability extends Component {
                                 <label className="Day tab-radius"><span>Thursday</span></label>
                                 <div className="border-left border-bottom border mobile-radius">
                                     <select
-                                        className="hours"
-                                        type="text">
+                                        className="form-control"
+                                        type="text"
+                                        value={this.state.thursday}
+                                        onChange={(event) => this.handleThursdayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
                                     </select>
@@ -81,8 +222,10 @@ class Availability extends Component {
                                 <label className="Day tab-radius"><span>Friday</span></label>
                                 <div className="border-left border-bottom border mobile-radius">
                                     <select
-                                        className="hours"
-                                        type="text">
+                                        className="form-control"
+                                        type="text"
+                                        value={this.state.friday}
+                                        onChange={(event) => this.handleFridayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
                                     </select>
@@ -92,8 +235,10 @@ class Availability extends Component {
                                 <label className="Day tab-radius"><span>Saturday</span></label>
                                 <div className="border-left border-bottom border mobile-radius">
                                     <select
-                                        className="hours"
-                                        type="text">
+                                        className="form-control"
+                                        type="text"
+                                        value={this.state.saturday}
+                                        onChange={(event) => this.handleSaturdayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
                                     </select>
@@ -103,16 +248,16 @@ class Availability extends Component {
                                 <label className="Day right-radius tab-radius"><span>Sunday</span></label>
                                 <div className="border-left border-bottom border-right border bottom-right-radius mobile-radius">
                                     <select
-                                        className="hours"
-                                        type="text">
+                                        className="form-control"
+                                        type="text"
+                                        value={this.state.sunday}
+                                        onChange={(event) => this.handleSundayChange(event)}>
                                         <option value="6AM - 2PM">6AM - 2PM</option>
                                         <option value="2PM - 10PM">2PM - 10PM</option>
                                     </select>
                                 </div>
                             </div>
-                            
                         </div>
-                        <button className="set-btn">Submit</button>
                     </div>
                 </div>
             </Aux>
