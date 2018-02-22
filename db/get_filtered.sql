adba604ea0fc9bc1b666317f1dd4ed7776a63ebb
Sergey Sherstobitov <in43sh@gmail.com>
2018-02-22 10:16:35 -0700

    changed sql request so we can see only the users, not their available days

--- db/get_filtered.sql
+++ db/get_filtered.sql
@@ -1,4 +1,4 @@
-SELECT * 
-FROM caregiver_availability ca
-JOIN users u ON ca.user_id = u.user_id
+SELECT DISTINCT u.user_id, u.first_name, u.last_name, u.street_address, u.state, u.city, u.zip, u.email, u.phone, u.avatar, u.title, u.longitude, u.latitude, u.about_message, u.proximity
+FROM users u
+JOIN caregiver_availability ca ON ca.user_id = u.user_id
 WHERE (begin_time < $1 AND end_time > $1) AND (u.proximity = $2);
--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,4 @@
-DROP TABLE IF EXISTS users, animals, jobs, review, caregiver_availability CASCADE;
-
+DROP TABLE IF EXISTS users, animals, jobs, reviews, caregiver_availability CASCADE;
 
 CREATE TABLE IF NOT EXISTS users (
     user_id SERIAL PRIMARY KEY,
@@ -23,7 +22,13 @@ CREATE TABLE IF NOT EXISTS users (
 INSERT INTO users
 (first_name, last_name, street_address, state, city, zip, email, phone, avatar, title, password, longitude, latitude, about_message, proximity)
 VALUES
-('Jemaine', 'Brown', 'Lane', 'Ca', 'Santa Monica', '90048', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'petowner', '1', '-118.390856', '34.095567', 'blablabla', 'bla');
+('Jemaine', 'Brown', 'Lane', 'Ca', 'Santa Monica', '90048', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'caregiver', '1', '-118.390856', '34.095567', 'blablabla', '3'),
+('Sergey', 'Sherstobitov', 'Lane', 'Ca', 'Santa Monica', '9813', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'caregiver', '1', '-118.390856', '34.095567', 'blablabla', '5'),
+('Jake', 'Jakeson', 'Lane', 'Ca', 'Santa Monica', '9813', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'caregiver', '1', '-118.390856', '34.095567', 'blablabla', '7'),
+('John', 'Johnson', 'Lane', 'Ca', 'Santa Monica', '9813', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'caregiver', '1', '-118.390856', '34.095567', 'blablabla', '10'),
+('Kate', 'Kateson', 'Lane', 'Ca', 'Santa Monica', '9813', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'caregiver', '1', '-118.390856', '34.095567', 'blablabla', '3'),
+('Jane', 'Janeson', 'Lane', 'Ca', 'Santa Monica', '9813', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'caregiver', '1', '-118.390856', '34.095567', 'blablabla', '5'),
+('Petowner', 'Petownerson', 'Lane', 'Ca', 'Santa Monica', '9813', 'mrmunster@gmail.com', '3236666666', 'https://vignette.wikia.nocookie.net/headhuntershorrorhouse/images/6/6b/Herman_Munster_001.jpg/revision/latest?cb=20091022161116', 'petowner', '1', '-118.390856', '34.095567', 'blablabla', '5');
 
 CREATE TABLE IF NOT EXISTS animals (
     animal_id SERIAL PRIMARY KEY,
@@ -44,27 +49,78 @@ VALUES
 CREATE TABLE IF NOT EXISTS caregiver_availability (
     caregiver_availability_id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users (user_id) NOT NULL,
-    day INTEGER,
-    begin_time VARCHAR (10),
-    end_time VARCHAR (10),
-    am_pm VARCHAR (5)
+    day INTEGER NOT NULL,
+    time_range VARCHAR (10),
+    begin_time INTEGER,
+    end_time INTEGER
 );
+
+INSERT INTO caregiver_availability
+(user_id, day, time_range, begin_time, end_time)
+VALUES
+(1, 0, '6AM - 2PM', 6, 14),
+(1, 1, '2PM - 10PM', 2, 10),
+(1, 2, '6AM - 2PM', 6, 14),
+(1, 3, '2PM - 10PM', 2, 10),
+(1, 4, '6AM - 2PM', 6, 14),
+(1, 5, '6AM - 2PM', 6, 14),
+(1, 6, '2PM - 10PM', 2, 10),
+(2, 0, '6AM - 2PM', 6, 14),
+(2, 1, '6AM - 2PM', 6, 14),
+(2, 2, '2PM - 10PM', 2, 10),
+(2, 3, '2PM - 10PM', 2, 10),
+(2, 4, '6AM - 2PM', 6, 14),
+(2, 5, '2PM - 10PM', 2, 10),
+(2, 6, '2PM - 10PM', 2, 10),
+(3, 0, '2PM - 10PM', 2, 10),
+(3, 1, '2PM - 10PM', 2, 10),
+(3, 2, '2PM - 10PM', 2, 10),
+(3, 3, '6AM - 2PM', 6, 14),
+(3, 4, '6AM - 2PM', 6, 14),
+(3, 5, '6AM - 2PM', 6, 14),
+(3, 6, '6AM - 2PM', 6, 14),
+(4, 0, '2PM - 10PM', 2, 10),
+(4, 1, '6AM - 2PM', 6, 14),
+(4, 2, '2PM - 10PM', 2, 10),
+(4, 3, '6AM - 2PM', 6, 14),
+(4, 4, '6AM - 2PM', 6, 14),
+(4, 5, '6AM - 2PM', 6, 14),
+(4, 6, '6AM - 2PM', 6, 14),
+(5, 0, '2PM - 10PM', 2, 10),
+(5, 1, '2PM - 10PM', 2, 10),
+(5, 2, '6AM - 2PM', 6, 14),
+(5, 3, '6AM - 2PM', 6, 14),
+(5, 4, '6AM - 2PM', 6, 14),
+(5, 5, '2PM - 10PM', 2, 10),
+(5, 6, '6AM - 2PM', 6, 14),
+(6, 0, '6AM - 2PM', 6, 14),
+(6, 1, '6AM - 2PM', 6, 14),
+(6, 2, '2PM - 10PM', 2, 10),
+(6, 3, '2PM - 10PM', 2, 10),
+(6, 4, '6AM - 2PM', 6, 14),
+(6, 5, '6AM - 2PM', 6, 14),
+(6, 6, '2PM - 10PM', 2, 10),
+(7, 0, '6AM - 2PM', 6, 14),
+(7, 1, '2PM - 10PM', 2, 10),
+(7, 2, '6AM - 2PM', 6, 14),
+(7, 3, '2PM - 10PM', 2, 10),
+(7, 4, '6AM - 2PM', 6, 14),
+(7, 5, '6AM - 2PM', 6, 14),
+(7, 6, '6AM - 2PM', 6, 14);
+
 
 CREATE TABLE IF NOT EXISTS jobs (
     job_id SERIAL PRIMARY KEY,
-    user_id INTEGER REFERENCES users (user_id) NOT NULL,
+    caregiver_id INTEGER REFERENCES users (user_id) NOT NULL,
+    petowner_id INTEGER REFERENCES users (user_id) NOT NULL,
     comments VARCHAR (100),
     month INTEGER NOT NULL,
     day INTEGER NOT NULL,
     year INTEGER NOT NULL,
     begin_time VARCHAR (10) NOT NULL,
     end_time VARCHAR (10) NOT NULL,
-    am_pm VARCHAR (5) NOT NULL,
-    animal_id INTEGER REFERENCES animals (animal_id) NOT NULL,
-    request_status VARCHAR (3) NOT NULL,
-    thirty_minute_service VARCHAR (3) NOT NULL,
-    sixty_minute_service VARCHAR (3) NOT NULL,
-    sixty_minute_park_service VARCHAR (3) NOT NULL
+    request_status BOOLEAN NOT NULL,
+    service VARCHAR(10) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
