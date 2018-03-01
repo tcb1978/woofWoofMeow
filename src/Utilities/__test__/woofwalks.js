6e81b3cc0e83cdc76a123a8b9e110777c78ee536
Matthew Eldredge <mattheweldredge@gmail.com>
2018-03-01 15:00:52 -0700

    unit tests

--- src/Utilities/__test__/woofwalks.js
+++ src/Utilities/__test__/woofwalks.js
@@ -1,6 +1,5 @@
 module.exports = {
     timeConvertion: function (time) {
-        // var time = '4:00 am';
         
         var hours = parseInt(time.substr(0, 2));
         if (time.indexOf('am') != -1 && hours == 12) {
@@ -10,5 +9,61 @@ module.exports = {
             time = time.replace(hours, (hours + 12));
         }
         return time.replace(/(am|pm)/, '');
+    },
+
+    checkMonday: function (monday) {
+        monday = 1
+        let date = new Date
+        if (date != monday) {
+            return true
+        }
+    },
+
+    checkTuesday: function (tuesday) {
+        tuesday = 2
+        let date = new Date
+        if (date != tuesday) {
+            return true
+        }
+    },
+
+    checkWednesday: function (wednesday) {
+        wednesday = 3
+        let date = new Date
+        if (date != wednesday) {
+            return wednesday
+        }
+    },
+
+    checkThursday: function (Thursday) {
+        Thursday = 4
+        let date = new Date
+        if (date != Thursday) {
+            return Thursday
+        }
+    },
+
+    checkFriday: function (Friday) {
+        Friday = 5
+        let date = new Date
+        if (date != Friday) {
+            return Friday
+        }
+    },
+
+    checkSaturday: function (Saturday) {
+        Saturday = 6
+        let date = new Date
+        if (date != Saturday) {
+            return Saturday
+        }
+    },
+
+    checkSunday: function (Sunday) {
+        Sunday = 7
+        let date = new Date
+        if (date != Sunday) {
+            return true
+        }
     }
 }
--- src/Utilities/__test__/woofwalks.test.js
+++ src/Utilities/__test__/woofwalks.test.js
@@ -11,3 +11,27 @@ test('Time convertion', () => {
 test('Time convertion', () => {
     expect(woofwalks.timeConvertion('10:30 pm')).toBe('22:30 ');
 })
+
+test('verify monday value is correct', () => {
+    expect(woofwalks.checkMonday(false)).toBe(true)
+})
+
+test('verify tuesday value is correct', () => {
+    expect(woofwalks.checkTuesday(true)).not.toBe(false)
+})
+
+test('verify wenesday value is correct', () => {
+    expect(woofwalks.checkWednesday(5)).toBe(3)
+})
+
+test('verify thursday value is correct', () => {
+    expect(woofwalks.checkThursday(5)).toBe(4)
+})
+
+test('verify friday value is correct', () => {
+    expect(woofwalks.checkFriday(6)).toBe(5)
+})
+
+test('verify saturday value is correct', () => {
+    expect(woofwalks.checkSaturday(7)).toBe(6)
+})
