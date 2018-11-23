2109363d281b1d92313adf8033234ea9c80a0f4b
Matthew Eldredge <mattheweldredge@gmail.com>
2018-11-23 09:07:26 -0800

    remove duplicate line

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,14 +5,14 @@ import './NotFound.css';
 const NotFound = () =>
 <div className="NotFound">
   <h3>Error 404</h3>
-  
+
   <p>
     We are sorry but the page you are looking for does not exist. <br />
     Please, come back to Home page.<br />
     <Link to="/">Home</Link>
   </p>
-  <img src="https://http.cat/404" alt="404 page not found" height="500" alt="404 not found"/>
-  
+  <img src="https://http.cat/404" height="500" alt="404 not found"/>
+
 </div>
 
 export default NotFound;
